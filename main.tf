c9682eaa33c4a04a1fb979c949c8b1c0648e9cf6
Justin Jose <justinjp333@gmail.com>
2026-02-26 21:28:18 -0500

    Update AMI ID for AWS instance

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_instance" "name" {
-  ami           = "ami-0b6c6ebed2801a5cb"
+  ami           = "ami-06e3c045d79fd65d9"
   instance_type ="t3.micro"
   tags = {
     name = "HCP-SERVER"
